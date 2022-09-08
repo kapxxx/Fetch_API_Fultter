37585e5fc071e48aff05042502212ed979135c7c
kalpesh <88702677+kapxxx@users.noreply.github.com>
2022-09-09 01:08:36 +0530

    single user data fetch from api

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+void main()
+{
+  runApp(myapp());
+}
+class myapp extends StatelessWidget {
+  const myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home(),);
+  }
+}
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  late Map mapresponse;
+  late Map dataresponse;
+  Future callApi() async{
+    Response response=await get(Uri.parse('https://reqres.in/api/users/2')); //this is used to fatch data from api
+    if(response.statusCode==200) //we are cheching status code
+      {
+        setState((){
+          print(response.body);
+          mapresponse =json.decode(response.body);
+          dataresponse=mapresponse['data'];
+        });
+      }
+  }
+
+  void initState(){
+    callApi();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('User data'),),
+    body: Container(
+      child: mapresponse==null?Text('loding'): Text(dataresponse['email'].toString()),),);
+
+  }
+}
