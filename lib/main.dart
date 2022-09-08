65824b24db15a67ecd7970f2e918eb8293c04a69
kalpesh <88702677+kapxxx@users.noreply.github.com>
2022-09-09 01:34:46 +0530

    fetch multiple data from api with image

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+
+//fetch multiple data from api for that
+// change url for api
+//   create list variable and
+//   used Listview.builder
+
+
 void main()
 {
   runApp(myapp());
@@ -25,14 +31,15 @@ class _HomeState extends State<Home> {
 
   late Map mapresponse;
   late Map dataresponse;
+  late List listitems;
   Future callApi() async{
-    Response response=await get(Uri.parse('https://reqres.in/api/users/2')); //this is used to fatch data from api
+    Response response=await get(Uri.parse('https://reqres.in/api/users?page=2')); //this is used to fatch data from api
     if(response.statusCode==200) //we are cheching status code
       {
         setState((){
           print(response.body);
           mapresponse =json.decode(response.body);
-          dataresponse=mapresponse['data'];
+          listitems=mapresponse['data'];
         });
       }
   }
@@ -43,8 +50,20 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title: Text('User data'),),
-    body: Container(
-      child: mapresponse==null?Text('loding'): Text(dataresponse['email'].toString()),),);
+    // body: Container(
+    //   child: listitems==null?Text('loding'): Text(listitems[0]['id'].toString()),),);
+    
+    body: ListView.builder(
+      itemCount: listitems.length,
+        itemBuilder: (context,index){
+      return Container(
+      child: Column(
+      children: [
+        Text(listitems[index]['email'].toString()),
+        Image.network(listitems[index]['avatar']),
 
+    ],),);
+    } ));
+    
   }
 }
